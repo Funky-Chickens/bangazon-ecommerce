4e2725be5eb8e2bbbc4f7401322e6fe5a938c23f
cait-rocco <cait.rocco@gmail.com>
2017-10-24 15:52:17 -0500

    made a route to categories which gets the categories and displays in pug

--- controllers/categoryCtrl.js
+++ controllers/categoryCtrl.js
@@ -3,15 +3,18 @@
 // getCategories //(with 1st 3 products and count of products in category)
 
 module.exports.getCategories = (req, res, next) => {
-    const { Categories } = req.app.get('models');
-    Categories.findAll() 
-    .then( (categories) => {
-      let cats = categories.map( (cat) => {
-        return cat.dataValues;
-      });
-      res.render('categories', {cats});
-    })
-    .catch( (err) => {
-      next(err); 
+  console.log("hi", req);
+  const { Category } = req.app.get('models');
+  console.log("category", Category);
+  Category.findAll() 
+  .then( (categories) => {
+    console.log("cats?", categories);
+    let cats = categories.map( (cat) => {
+      return cat.dataValues;
     });
-  };
+    res.render('categories', {cats});
+  })
+  .catch( (err) => {
+    next(err); 
+  });
+};
